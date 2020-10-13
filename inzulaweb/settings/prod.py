e19ccecab21a337eea65a215f1d6d5ff8d9d8a17
mikael19 <mikaelsondi@gmail.com>
2020-10-13 21:38:52 +0200

    prod settings

--- inzulaweb/settings/prod.py
+++ inzulaweb/settings/prod.py
@@ -41,6 +41,12 @@ AUTH_PASSWORD_VALIDATORS = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8001',
-    'http://15.188.83.142:8001',
     'http://127.0.0.1:8001',
+    'http://15.188.83.142:8001',
+    'http://172.26.15.28:8001/',
+    'http://localhost:5000',
+    'http://127.0.0.1:5000',
+    'http://15.188.83.142:5000',
+    'http://172.26.15.28:5000/',
+
 )
