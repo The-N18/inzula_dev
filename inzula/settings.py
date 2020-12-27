f1ad56a814d91e2f0b0b33b82d99a8daf8aad216
mikael19 <mikaelsondi@gmail.com>
2020-12-27 19:54:35 +0100

    allowed

--- inzula/settings.py
+++ inzula/settings.py
@@ -188,7 +188,7 @@ ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Email parameters
-EMAIL_HOST_URL = 'http://52.47.76.117:5000/verify/'
+EMAIL_HOST_URL = 'http://inzula.app/verify/'
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULTFROMEMAIL", 'inzula.app@gmail.com')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
