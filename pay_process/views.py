2e059812919c8a99cf53bf0ef64d7dc94570e2ca
mikael19 <mikaelsondi@gmail.com>
2021-01-02 01:57:23 +0100

    Deploy 1-1-20 5

--- pay_process/views.py
+++ pay_process/views.py
@@ -141,7 +141,7 @@ class PayForBooking(CreateAPIView):
                         debited_funds=Money(amount=fees_amount, currency='EUR'),
                         fees=Money(amount=0, currency='EUR'),
                         debited_wallet=user_wallet,
-                        credited_wallet=inzula_wallet)
+                        credited_wallet_id=admin_user.wallet_id)
             transfer.save()
 
             # change the status of all bookings and create notifications
