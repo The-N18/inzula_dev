96eb38e8ee12d2aabfe9e9d10dfe728c72ab7d3b
mikael19 <mikaelsondi@gmail.com>
2021-01-17 21:15:59 +0100

    Updates 17 Jan

--- bin/gunicorn_start.sh
+++ bin/gunicorn_start.sh
@@ -11,6 +11,7 @@ USER=ubuntu
 GROUP=ubuntu
 # how many worker processes should Gunicorn spawn
 NUM_WORKERS=3
+TIMEOUT=120
 # which settings file should Django use
 DJANGO_SETTINGS_MODULE=inzula.settings
 # WSGI module name
@@ -29,6 +30,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 exec ../env/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
 --name $NAME \
 --workers $NUM_WORKERS \
+--timeout $TIMEOUT \
 --user=$USER --group=$GROUP \
 --bind=unix:$SOCKFILE \
 --log-level=debug \
