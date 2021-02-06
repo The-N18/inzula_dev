067afbf1a60c40529b0c7fd8f7805aa7ecb98e8a
mikael19 <mikaelsondi@gmail.com>
2021-02-07 00:52:09 +0100

    scripts

--- bin/build_in_prod_server.sh
+++ bin/build_in_prod_server.sh
@@ -2,7 +2,10 @@
 
 # replace server url in config file
 config_file='/home/ubuntu/projects/inzula/src/configurations.js'
+settings_file='/home/ubuntu/projects/inzula/inzula/settings.py'
+
 sed -i 's/d1g0ix8w0r103u.cloudfront.net/dkx1b8wlo613w.cloudfront.net/' config_file
+sed -i 's/INZ_TEST_DB_NAME/INZ_DB_NAME/' settings_file
 
 #execute build command for the frontend
 PUBLIC_URL=https://dkx1b8wlo613w.cloudfront.net npm run build && ./gunicorn_start.sh
--- bin/build_in_test_server.sh
+++ bin/build_in_test_server.sh
@@ -2,7 +2,10 @@
 
 # replace server url in config file
 config_file='/home/ubuntu/projects/inzula/src/configurations.js'
+settings_file='/home/ubuntu/projects/inzula/inzula/settings.py'
+
 sed -i 's/dkx1b8wlo613w.cloudfront.net/d1g0ix8w0r103u.cloudfront.net/' config_file
+sed -i 's/INZ_DB_NAME/INZ_TEST_DB_NAME/' settings_file
 
 #execute build command for the frontend
 PUBLIC_URL=https://d1g0ix8w0r103u.cloudfront.net npm run build && ./gunicorn_start.sh
