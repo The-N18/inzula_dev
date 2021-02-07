fa91fd1c1c17f94290233b7c0c7d44aad1529503
inzuladev <tenteesarl@gmail.com>
2021-02-07 01:08:09 +0000

    script updates

--- bin/build_in_test_server.sh
+++ bin/build_in_test_server.sh
@@ -7,5 +7,4 @@ settings_file='/home/ubuntu/projects/inzula/inzula/settings.py'
 sed -i 's/dkx1b8wlo613w.cloudfront.net/d1g0ix8w0r103u.cloudfront.net/' $config_file
 sed -i 's/INZ_DB_NAME/INZ_TEST_DB_NAME/' $settings_file
 
-#execute build command for the frontend
-PUBLIC_URL=https://d1g0ix8w0r103u.cloudfront.net npm run build && python3 /home/ubuntu/projects/inzula/manage.py collectstatic --noinput && ./gunicorn_start.sh
+rm -rf /home/ubuntu/projects/inzula/build/ && PUBLIC_URL=https://d1g0ix8w0r103u.cloudfront.net npm run build && python3 /home/ubuntu/projects/inzula/manage.py collectstatic --noinput && ./gunicorn_start.sh
