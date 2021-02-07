620c5954566d3905147354ef94b3f89a245316b7
inzuladev <tenteesarl@gmail.com>
2021-02-07 02:09:29 +0000

    scripts

--- bin/build_in_test_server.sh
+++ bin/build_in_test_server.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 # replace server url in config file
-config_file='/home/ubuntu/projects/inzula/src/configurations.js'
-settings_file='/home/ubuntu/projects/inzula/inzula/settings.py'
+config_file='/home/ubuntu/projects/inzula/src/configurations.js';
+settings_file='/home/ubuntu/projects/inzula/inzula/settings.py';
 
-sed -i 's/dkx1b8wlo613w.cloudfront.net/d1g0ix8w0r103u.cloudfront.net/' $config_file
-sed -i 's/INZ_DB_NAME/INZ_TEST_DB_NAME/' $settings_file
+sed -i 's/dkx1b8wlo613w.cloudfront.net/d1g0ix8w0r103u.cloudfront.net/' $config_file;
+sed -i 's/INZ_DB_NAME/INZ_TEST_DB_NAME/' $settings_file;
 
-rm -rf /home/ubuntu/projects/inzula/build/ && rm -rf /home/ubuntu/projects/inzula/staticfiles/ && PUBLIC_URL=https://d1g0ix8w0r103u.cloudfront.net && npm run build && python3 /home/ubuntu/projects/inzula/manage.py collectstatic --noinput && ./home/ubuntu/projects/inzula/bin/gunicorn_start.sh
+rm -rf /home/ubuntu/projects/inzula/build/;
+rm -rf /home/ubuntu/projects/inzula/staticfiles/;
+PUBLIC_URL='https://d1g0ix8w0r103u.cloudfront.net';
+source ~/.nvm/nvm.sh;
+npm run build;
+python3 /home/ubuntu/projects/inzula/manage.py collectstatic --noinput;
+/home/ubuntu/projects/inzula/bin/gunicorn_start.sh;
+
